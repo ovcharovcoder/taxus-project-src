c9739b3722371d3ef6ed366057c4a96ba8344ffc
OvcharovCoder <ovcharovcoder@gmail.com>
2025-01-11 16:20:31 +0200

    Added Reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
 // Scripts are indicated there
+const swiper = new Swiper('.reviews__testimonials-swiper', {
+	slidesPerView: 3,
+	spaceBetween: 20,
+	freeMode: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
