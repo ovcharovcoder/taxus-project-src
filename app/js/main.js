f3bbb2c4b02186084681c78fbaf494674e13c540
OvcharovCoder <ovcharovcoder@gmail.com>
2025-01-13 18:08:40 +0200

    Added Banking paga

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
+// Toggle tables visibility using button on 'Clients Fees' section 'banking.html' page
 
+function showContent(tableId, buttonElement) {
+	document.querySelectorAll('.fees__content').forEach(content => {
+		content.classList.remove('active');
+	});
+	document.getElementById(tableId).classList.add('active');
+
+	document.querySelectorAll('.fees__button-group button').forEach(button => {
+		button.classList.remove('active');
+	});
+	buttonElement.classList.add('active');
+}
